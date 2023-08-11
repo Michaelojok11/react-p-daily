98c46e2e100d2a3cabb6c6ea0c33c8fb164c984a
Michaelojok11 <michaelocan11@gmail.com>
2023-08-11 09:10:38 +0300

    update git

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 // react components
 
 
-
 // props allows you to pass data through dynamic components of react
 
 
 const App = () => { 
+  // hook
+  const [counter,setCounter] = useState(0);
   return (
     <div className="App">
-    <button> - </button>
-    <h1> 0 </h1>
-    <button> + </button>
-   
+    <button onClick={() => setCounter((prevCount) => prevCount-1)}> - </button>
+  
+    <h1> {counter} </h1>
+    <button onClick={() => setCounter((prevCount) => prevCount+1)}> + </button>
+              
     </div>
   );
 }
